cd6a46d4c2cb0c41b20b39915c72f1c882630ade
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-11 09:39:00 +0300

    Add checkmark accessory to cell

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -32,7 +32,13 @@ class TodoListViewController: UITableViewController {
     //MARK - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(itemArray[indexPath.row])
+//        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
