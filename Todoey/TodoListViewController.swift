12cc3facecc12c200dc38a308b29fc65cfc4591b
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-11 10:22:42 +0300

    Add bar button and connect it

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -37,13 +37,17 @@ class TodoListViewController: UITableViewController {
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+            tableView.cellForRow(at: indexPath)?.accessoryType  = .checkmark
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
-
+    //MARK - Add New Items
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
 
 }
 
