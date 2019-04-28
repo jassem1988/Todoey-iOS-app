7d5c7916f56a71ff7e7c1997553da6cc2fe9052a
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-28 11:25:33 +0300

    Change:- - categories var to an optional Results - numberOfRowInSection func to return 1 if nil - cellForRowAt func to set text to a string if nil - fix loadCategories func to load from realm.objects

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -14,31 +14,25 @@ class CategoryViewController: UITableViewController {
     
     let realm = try! Realm()
     
-    var categories = [Category]()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var categories: Results<Category>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+       loadCategories()
         
-//        let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
-//        print(dataFilePath)
-        
-//       loadCategories()
- 
     }
     
     //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
+        return categories?.count ?? 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
         
-        cell.textLabel?.text = categories[indexPath.row].name
+        cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
         
         return cell
     }
@@ -55,7 +49,7 @@ class CategoryViewController: UITableViewController {
         let destinationVC = segue.destination as! TodoListViewController
         
         if let indexPath = tableView.indexPathForSelectedRow {
-            destinationVC.selectedCategory = categories[indexPath.row]
+            destinationVC.selectedCategory = categories?[indexPath.row]
             
         }
         
@@ -76,15 +70,12 @@ class CategoryViewController: UITableViewController {
         tableView.reloadData()
     }
     
-//    func loadCategories(with request: NSFetchRequest<Category> = Category.fetchRequest()) {
-//        do {
-//            categories = try context.fetch(request)
-//        } catch {
-//            print("Error fetching context, \(error)")
-//        }
-//        
-//        tableView.reloadData()
-//    }
+    func loadCategories() {
+        
+        categories = realm.objects(Category.self)
+        
+        tableView.reloadData()
+    }
     
     //MARK: - Add new Categories
     
@@ -99,8 +90,6 @@ class CategoryViewController: UITableViewController {
             guard let textFieldText = textField.text else { return }
             
             newCategory.name = textFieldText
-            
-            self.categories.append(newCategory)
             
             self.save(category: newCategory)
             
