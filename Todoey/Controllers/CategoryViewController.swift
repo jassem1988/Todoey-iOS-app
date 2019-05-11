125d7f144b31cc1b5208925e908b34e14c15dec1
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-05-11 22:09:54 +0300

    - Install Chameleon Framework - Add a color properties in the Category model - create random color for each new category - change cell background to the new color

--- Todoey/Controllers/CategoryViewController.swift
+++ Todoey/Controllers/CategoryViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import RealmSwift
+import ChameleonFramework
 
 class CategoryViewController: SwipeTableViewController {
     //Properties
@@ -20,6 +21,8 @@ class CategoryViewController: SwipeTableViewController {
         super.viewDidLoad()
         
         loadCategories()
+        
+        tableView.separatorStyle = .none
         
     }
     
@@ -35,6 +38,8 @@ class CategoryViewController: SwipeTableViewController {
         let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         cell.textLabel?.text = categories?[indexPath.row].name ?? "No Categories Added Yet"
+        
+        cell.backgroundColor = UIColor(hexString: categories?[indexPath.row].color ?? "65ACCF")
         
         return cell
     }
@@ -109,6 +114,7 @@ class CategoryViewController: SwipeTableViewController {
             guard let textFieldText = textField.text else { return }
             
             newCategory.name = textFieldText
+            newCategory.color = UIColor.randomFlat.hexValue()
             
             self.save(category: newCategory)
             
